f52cdd11cbf63e4782fd3537a8ef6121556fd689
Mateo <mateo10velasquez@hotmail.com>
2020-03-08 15:57:09 -0500

    Agregados algunos metadatos

---
+++ cnn/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Pendiente"
+__copyright__ = "2020"
+__credits__ = ["Pablo Carmona", "Sara Hernandez",
+               "Mateo Velásquez", "Brayhan Zuluaga"]
+__license__ = "Propietary"
+__version__ = "20200206"
+__maintainer__ = "Mateo Velásquez"
+__email__ = "mavemo26049@gmail.com"
+__status__ = "Development"
