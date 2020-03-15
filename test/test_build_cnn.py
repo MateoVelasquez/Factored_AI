6622616e981270f187dd1a204661a923aa0aaaf5
Mateo <mateo10velasquez@hotmail.com>
2020-03-15 15:50:05 -0500

    Agregado modulo de prediccion

--- test/test_build_cnn.py
+++ test/test_build_cnn.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 ruta = os.path.abspath(os.path.normpath('..'))
 sys.path.append(ruta)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -16,7 +19,7 @@ cfg_train = {
 }
 
 
-def test_red():
+def train_red():
     # Parametros de configuracion de la red
     parametros = {
         'padding': (4, 4),
@@ -37,5 +40,31 @@ def test_red():
     red.resultados_graficos(historial)
 
 
+def prediccion(red_name, img_to_predict):
+    cnn = load_model(os.path.join(ruta, 'saved_models', red_name + '.h5'))
+    cnn.load_weights(os.path.join(ruta, 'saved_models',
+                                  red_name + '_pesos.h5'))
+
+    img = load_img(img_to_predict, target_size=(cfg_train['IMG_SZE'][1],
+                                                cfg_train['IMG_SZE'][0]),
+                   color_mode=cfg_train['IMG_COLOR'])
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    arreglo = cnn.predict(img)
+    resultado = arreglo[0]
+    print('\n')
+    if resultado[0] < 0.4:
+        print(f'Probabilidad {resultado[0]}. El paciente no tiene neumonia')
+    else:
+        print(f'Probabilidad {resultado[0]}. El paciente pude tener neumonia')
+
+
 if __name__ == "__main__":
-    test_red()
+
+    # # crear y entrenar la red:
+    # train_red()
+
+    # # hacer una prediccion:
+    file_path = r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\val\PNEUMONIA\person1952_bacteria_4883.jpeg' #noqa
+    # file_path = r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\val\NORMAL\NORMAL2-IM-1436-0001.jpeg' #noqa
+    prediccion('alexnet', file_path)
