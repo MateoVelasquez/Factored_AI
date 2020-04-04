2058a8f235a0c2bff0372bd8b88d651a306e7f2d
Mateo <mateo10velasquez@hotmail.com>
2020-04-04 09:50:32 -0500

    Agregada funcion evaluate

--- test/test_build_cnn.py
+++ test/test_build_cnn.py
@@ -7,13 +7,14 @@ ruta = os.path.abspath(os.path.normpath('..'))
 sys.path.append(ruta)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import cnn.red_neural as red #noqa
+from cnn.gen_generator import build_generador #noqa
 
 cfg_train = {
     'TRAIN_DATA_PATH': r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\train',
     'TEST_DATA_PATH': r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\test',
     'VAL_DATA_PATH': r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\val',
     'BATCH_SIZE': 1,
-    'EPOCAS': 60,
+    'EPOCAS': 50,
     'IMG_SZE': (200, 200, 3),  # (heigth, width, channel)
     'IMG_COLOR': "rgb"
 }
@@ -51,12 +52,30 @@ def prediccion(red_name, img_to_predict):
     img = img_to_array(img)
     img = np.expand_dims(img, axis=0)
     arreglo = cnn.predict(img)
-    resultado = arreglo[0]
-    print('\n')
-    if resultado[0] < 0.4:
-        print(f'Probabilidad {resultado[0]}. El paciente no tiene neumonia')
-    else:
-        print(f'Probabilidad {resultado[0]}. El paciente pude tener neumonia')
+    print(arreglo)
+    # resultado = arreglo[0]
+    # print('\n')
+    # if resultado[0] < 0.4:
+    #     print(f'Probabilidad {resultado[0]}. El paciente no tiene neumonia')
+    # else:
+    #     print(f'Probabilidad {resultado[0]}. El paciente pude tener neumonia')
+
+
+def evaluar(red_name):
+    cnn = load_model(os.path.join(ruta, 'saved_models', red_name + '.h5'))
+    cnn.load_weights(os.path.join(ruta, 'saved_models',
+                                  red_name + '_pesos.h5'))
+    
+    dim = cfg_train['IMG_SZE'][:-1]
+
+    val_generador, _ = build_generador(cfg_train['VAL_DATA_PATH'],
+                                       batch_sze=cfg_train['BATCH_SIZE'],
+                                       trgt_sze=dim,
+                                       color=cfg_train['IMG_COLOR'])
+    result = cnn.evaluate(val_generador)
+    dict(zip(cnn.metrics_names, result))
+    pass
+
 
 
 def model_summary():
@@ -70,9 +89,12 @@ if __name__ == "__main__":
     #model_summary()
 
     # # crear y entrenar la red:
-    train_red()
+    #train_red()
 
-    # # # hacer una prediccion:
-    # file_path = r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\val\PNEUMONIA\person1952_bacteria_4883.jpeg' #noqa
-    # # file_path = r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\val\NORMAL\NORMAL2-IM-1436-0001.jpeg' #noqa
-    # prediccion('alexnet', file_path)
+    # # Evaluate
+    #evaluar('paper')
+
+    # # hacer una prediccion:
+    #file_path = r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\val\PNEUMONIA\person1952_bacteria_4883.jpeg' #noqa
+    file_path = r'C:\Users\MATEO\RepositoriosGIT\Factored_AI\img\val\NORMAL\NORMAL2-IM-1437-0001.jpeg' #noqa
+    prediccion('paper', file_path)
